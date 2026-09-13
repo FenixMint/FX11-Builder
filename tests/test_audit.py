323c7449573f9f2dc8153dba7e82bd8b31bca036
FenixMint <mgut@poczta.fm>
2026-09-13 15:58:43 +0200

    Test deep WIM inventory comparison

--- tests/test_audit.py
+++ tests/test_audit.py
@@ -1,4 +1,4 @@
-from fx11.audit import compare_inventories
+from fx11.audit import compare_inventories, compare_wim_inventories
 
 
 def test_compare_inventories_reports_added_removed_and_common():
@@ -26,3 +26,17 @@ def test_compare_inventories_detects_removed_source_file():
     assert delta.added == ("/c",)
     assert delta.removed == ("/a",)
     assert delta.common_count == 1
+
+
+def test_compare_wim_inventories_can_prove_path_inventory_is_unchanged():
+    files = ("/Windows/System32/kernel32.dll", "/Windows/explorer.exe", "/Users/Default/NTUSER.DAT")
+    delta = compare_wim_inventories(files, files)
+    assert delta.added == ()
+    assert delta.removed == ()
+    assert delta.common_count == 3
+
+
+def test_compare_wim_inventories_flags_unexpected_offline_change():
+    delta = compare_wim_inventories(("/Windows/a.dll", "/Windows/b.dll"), ("/Windows/a.dll", "/Windows/evil.dll"))
+    assert delta.added == ("/Windows/evil.dll",)
+    assert delta.removed == ("/Windows/b.dll",)
